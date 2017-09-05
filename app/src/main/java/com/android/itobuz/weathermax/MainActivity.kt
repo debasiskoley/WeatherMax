3fa503f3298653c74618c877b312498b0569bebb
debasis <deba.m20@gmail.com>
2017-09-05 16:48:47 +0530

    weather app fuction worked

--- app/src/main/java/com/android/itobuz/weathermax/MainActivity.kt
+++ app/src/main/java/com/android/itobuz/weathermax/MainActivity.kt
@@ -1,21 +1,47 @@
 package com.android.itobuz.weathermax
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import io.vrinda.kotlinpermissions.PermissionCallBack
+import io.vrinda.kotlinpermissions.PermissionsActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : PermissionsActivity() {
 
     private var cityName: EditText? = null
+    private var mFusedLocationClient: FusedLocationProviderClient? = null
 
+    //@SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-       cityName = findViewById(R.id.city_name)
+
+        my_location.setOnClickListener {
+                requestPermissions(Manifest.permission.ACCESS_FINE_LOCATION, object : PermissionCallBack {
+                    override fun permissionGranted() {
+                        super.permissionGranted()
+//                        Log.v("Location Permission", "Granted")
+
+                    }
+
+                    override fun permissionDenied() {
+                        super.permissionDenied()
+                        Log.v("Location Permission", "Denied")
+                    }
+                })
+
+        }
+
+        cityName = findViewById(R.id.city_name)
         val bundle = Bundle()
 
         submit_city.setOnClickListener {
@@ -38,4 +64,3 @@ class MainActivity : AppCompatActivity() {
         startActivity(i)
     }
 }
-
